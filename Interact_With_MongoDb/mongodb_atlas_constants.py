c0017b99ceae4cf05b85e4b547ca554c371cd4f3
Mitra <mayukh.mitra@thomsonreuters.com>
2024-04-06 14:04:12 +0530

    Python: Interact With MongoDB Atlas

---
+++ Interact_With_MongoDb/mongodb_atlas_constants.py
@@ -0,0 +1,4 @@
+""" Constants for MongoDB Atlas """
+
+MONGODB_ATLAS_ADMIN_URL = "mongodb+srv://{username}:{password}@demo.sglpxsj.mongodb.net/?retryWrites=true&w=majority&appName=DEMO"
+MONGODB_ATLAS_URL = "mongodb+srv://{username}:{password}@demo.sglpxsj.mongodb.net/"
